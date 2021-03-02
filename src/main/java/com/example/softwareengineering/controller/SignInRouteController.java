92dfda28cda509055ba1a9ddd5906f4f841ef021
Deuce Black <deuce2black@gmail.com>
2021-03-02 16:28:53 -0600

    Created backend user ID/password validation

--- src/main/java/com/example/softwareengineering/controller/SignInRouteController.java
+++ src/main/java/com/example/softwareengineering/controller/SignInRouteController.java
@@ -1,7 +1,31 @@
 package com.example.softwareengineering.controller;
+import com.example.softwareengineering.entity.EmployeeSignIn;
+import com.example.softwareengineering.repository.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.Response;
 
+@RestController
+@RequestMapping(value = "/")
 public class SignInRouteController {
-    public SignInRouteController()
+    private final EmployeeRepository repo;
+
+    public SignInRouteController(EmployeeRepository repo)
     {
+        this.repo = repo;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public ResponseEntity<Boolean> VerifyCreds(@RequestParam EmployeeSignIn empSignIn, @RequestParam HttpServletRequest reqId){
+        if(repo.findById(empSignIn.getEmployeeID()).get().getPassword().equals(empSignIn.getPassword())){
+            if(repo.findById(empSignIn.getEmployeeID()).get().getRole().equals("Manager")) {
+                return new ResponseEntity(true, HttpStatus.OK);
+            } else return new ResponseEntity(false, HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.UNAUTHORIZED);
     }
 }
