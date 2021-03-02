a86524ba89a2cbf330a8fe9e8af4490f044412da
Deuce Black <deuce2black@gmail.com>
2021-03-02 17:40:46 -0600

    some optional thing

--- src/main/java/com/example/softwareengineering/repository/EmployeeRepository.java
+++ src/main/java/com/example/softwareengineering/repository/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.example.softwareengineering.repository;
 import com.example.softwareengineering.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Optional<Employee> findById(int id);
 }
