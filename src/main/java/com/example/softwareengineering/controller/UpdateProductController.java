6ec191814bf137e76993ec31514d2989704bf34b
Keifer-Schmueser <52268060+Keifer-Schmueser@users.noreply.github.com>
2021-04-08 13:56:25 -0500

    Created update endpoint

--- src/main/java/com/example/softwareengineering/controller/UpdateProductController.java
+++ src/main/java/com/example/softwareengineering/controller/UpdateProductController.java
@@ -1,28 +1,41 @@
 package com.example.softwareengineering.controller;
 import com.example.softwareengineering.entity.Employee;
+import com.example.softwareengineering.entity.Products;
 import com.example.softwareengineering.repository.EmployeeRepository;
+import com.example.softwareengineering.repository.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin(origins = "*")
 @RestController
 public class UpdateProductController {
-    private final ProductRepository repo;
+    private final ProductsRepository repo;
 
     @Autowired
 
-    public UpdateProductController(ProductRepository repo) {
+    public UpdateProductController(ProductsRepository repo) {
         this.repo = repo;
     }
 
     @RequestMapping(value = "/products/update", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity UpdateProduct(@RequestParam(name = "id") String id, @RequestBody Product prod) {
-        Product oldProd = repo.findById(id);
-        
+    public ResponseEntity<Products> UpdateProduct(@RequestBody Products prod) {
+        if (repo.findById(prod.getProductID()).isPresent()){
+            Products oldProd = repo.findById(prod.getProductID()).get();
+            oldProd.setName(prod.getName());
+            oldProd.setImage(prod.getImage());
+            oldProd.setPrice(prod.getPrice());
+            repo.save(oldProd);
+            return new ResponseEntity(oldProd, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
 
